a4c3b09fd306ab9f31c752102c0697d87a3f3530
bzuker <brian@inorbit.ai>
2020-09-13 11:03:18 -0300

    done

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,31 @@
 import Layout from "../components/layout";
 import { useState } from 'react';
+import { getQuestions } from "../lib/getQuestions";
 
-function TextQuestion({ text, answer, onCorrect = () => null }) {
+function TextQuestion({ text, answer, image, onCorrect = () => null }) {
   const [response, setResponse] = useState('');
   const [error, setError] = useState(false);
 
-  function handleClick(e) {
-    if (response.toLowerCase() !== answer.toLowerCase()) {
+  function handleClick() {
+    const parsedResponse = response.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+    const parsedAnswer = answer.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+    if (parsedResponse.toLowerCase() !== parsedAnswer.toLowerCase()) {
       setError(true);
       return;
     }
 
     setError(false);
+    setResponse('');
     onCorrect();
   }
 
   return (
     <div className="flex flex-col items-center justify-center">
-      <h2 className="p-3 text-lg font-bold bg-yellow-400 md:text-4xl text-center">
+      <h2 className="p-3 text-lg font-bold bg-yellow-400 md:text-5xl text-center">
         {text}
       </h2>
 
+      {image && <img src={image} className="max-w-md"/>}
 
       <div className="flex flex-col items-center justify-center w-full my-4">
         <input
@@ -46,7 +51,7 @@ function TextQuestion({ text, answer, onCorrect = () => null }) {
   );
 }
 
-function MultipleChoiceQuestion({ text, options, answer, onCorrect = () => null }) {
+function MultipleChoiceQuestion({ text, options, answer, image, onCorrect = () => null }) {
   const [error, setError] = useState(false);
 
   function handleClick(e) {
@@ -61,9 +66,11 @@ function MultipleChoiceQuestion({ text, options, answer, onCorrect = () => null 
   }
   return (
     <div className="flex flex-col items-center justify-center">
-      <h2 className="p-3 text-lg font-bold bg-yellow-400 md:text-4xl text-center">
+      <h2 className="p-3 text-lg font-bold bg-yellow-400 md:text-5xl text-center">
         {text}
       </h2>
+
+      {image && <img src={image} className="max-w-md"/>}
 
       {error && <h2 className="p-3 text-lg font-bold bg-red-400 md:text-4xl text-center my-4">
         Casi! Probá de nuevo
@@ -81,76 +88,80 @@ function MultipleChoiceQuestion({ text, options, answer, onCorrect = () => null 
   );
 }
 
-function WelcomeMessage({ startGame }) {
+function Message({ text, onClick, buttonText = "Empezar" }) {
   return (
     <>
       <div className="flex flex-col items-center justify-center">
-        <h2 className="p-3 text-lg font-bold bg-yellow-400 md:text-4xl">
-          Hola! Vamos a contestar algunas preguntas
+        <h2 className="p-3 text-lg font-bold bg-yellow-400 md:text-5xl text-center">
+          {text}
         </h2>
       </div>
       <div className="flex flex-col items-center justify-center my-20">
         <button
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold text-4xl py-2 px-4 rounded"
-          onClick={() => startGame()}
+          onClick={() => onClick()}
         >
-          Empezar
+          {buttonText}
         </button>
       </div>
     </>
   );
 }
 
-function Index() {
-  const [questions, setQuestions] = useState([
-    {
-      text: "Cómo se llama tu nieto mayor?",
-      type: "multiple-choice",
-      options: [
-        { text: "Brian" },
-        { text: "Damián" },
-        { text: "Julián" },
-      ],
-      answer: "Damián",
-    },
-    {
-      text: "Completá la frase: Billetera mata...",
-      type: "text",
-      answer: "galan"
-    }
-  ]);
+function renderQuestion(question, onCorrect, startGame) {
+  if (!question) {
+    return <Message text="Se acabaron las preguntas!" buttonText="Volver a jugar" onClick={startGame} />
+  }
+
+  if (question.type === 'multiple-choice') {
+    return <MultipleChoiceQuestion {...question} onCorrect={onCorrect} />
+  }
+
+  if (question.type === 'text') {
+    return <TextQuestion {...question} onCorrect={onCorrect} />;
+  }
+}
+
+// Gets a random question
+function getNextQuestion(questions) {
+  const index = Math.floor(Math.random() * questions.length);
+  return questions[index];
+}
+
+function Index({ questions: remoteQuestions }) {
+  const [questions, setQuestions] = useState(remoteQuestions);
   const [started, setStarted] = useState(false);
   const [currentQuestion, setCurrentQuestion] = useState(null);
   
   function startGame() {
-    const index = Math.floor(Math.random() * questions.length);
-    const question = questions[index];
+    const question = getNextQuestion(remoteQuestions);
     setStarted(true);
     setCurrentQuestion(question);
-    setQuestions(questions.filter(x => x.text !== question.text))
+    setQuestions(remoteQuestions.filter(x => x.text !== question.text));
   }
 
-  function renderQuestion(question) {
-    if (!question) {
-      return;
-    }
-
-    if (question.type === 'multiple-choice') {
-      return <MultipleChoiceQuestion {...question} />
-    }
-
-    if (question.type === 'text') {
-      return <TextQuestion {...question} />;
-    }
+  function onCorrectGuess() {
+    const question = getNextQuestion(questions);
+    setCurrentQuestion(question);
+    setQuestions(questions.filter(x => x.text !== question.text));
   }
 
-  console.log({questions})
   return (
     <Layout>
-      { !started && <WelcomeMessage startGame={startGame} /> }
-      {renderQuestion(currentQuestion)}
+      { !started && <Message text="Hola! Vamos a contestar algunas preguntas" onClick={startGame} /> }
+      {started && renderQuestion(currentQuestion, onCorrectGuess, startGame)}
     </Layout>
   );
+}
+
+export async function getStaticProps(context) {
+  const questions = await getQuestions();
+  return {
+    props: {
+      questions
+    },
+    revalidate: 5
+  }
 }
 
 export default Index;
